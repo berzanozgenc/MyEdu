9f72ff1baca946822a114a080c803db9631e2f4c
berzanozgenc <berzanozgenc@gmail.com>
2024-05-22 02:03:18 +0300

    admin bug fixes

--- src/main/java/com/myEdu/ws/model/Admin.java
+++ src/main/java/com/myEdu/ws/model/Admin.java
@@ -1,7 +1,9 @@
 package com.myEdu.ws.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 
+@Entity
 public class Admin extends User {
 
 }
--- src/main/java/com/myEdu/ws/service/AdminService.java
+++ src/main/java/com/myEdu/ws/service/AdminService.java
@@ -4,19 +4,24 @@ import com.myEdu.ws.model.Admin;
 import com.myEdu.ws.model.Student;
 import com.myEdu.ws.repository.AdminRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class AdminService {
 
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    @Autowired
     private final AdminRepository adminRepository;
+
+    @Autowired
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
 
     public Admin createAdmin(Admin adminDto) {
         String encodedPassword = passwordEncoder.encode(adminDto.getPassword());
